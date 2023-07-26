7d9b309bf4ba449e35c845defc2a21e06b0cd38d
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2023-07-26 18:13:01 +0100

    create the admin_store_screen

--- lib/views/admin/admin_store_screen.dart
+++ lib/views/admin/admin_store_screen.dart
@@ -47,7 +47,7 @@ class AdminStoreScreen extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 16),
               child: Row(
                 children: [
-                  Icon(Icons.location_on),
+                  Icon(Icons.location_on, color: AppColors.accentClr),
                   SizedBox(width: 8),
                   Text(
                     'Store Address, City, Country', // Replace with your actual store address
@@ -63,10 +63,26 @@ class AdminStoreScreen extends StatelessWidget {
               padding: EdgeInsets.symmetric(horizontal: 16),
               child: Row(
                 children: [
-                  Icon(Icons.phone),
+                  Icon(Icons.phone, color: AppColors.accentClr),
                   SizedBox(width: 8),
                   Text(
                     '+123-456-7890', // Replace with your actual store phone number
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Icon(Icons.email, color: AppColors.accentClr),
+                  SizedBox(width: 8),
+                  Text(
+                    'exemple@appointify.com', // Replace with your actual store phone number
                     style: TextStyle(
                       fontSize: 16,
                     ),
@@ -78,7 +94,7 @@ class AdminStoreScreen extends StatelessWidget {
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 16),
               child: Text(
-                'Days of Work', // Add the heading for days of work
+                'Store Hours', // Add the heading for store hours
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
@@ -88,45 +104,68 @@ class AdminStoreScreen extends StatelessWidget {
             const SizedBox(height: 8),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Wrap(
-                spacing: 8,
+              child: Row(
                 children: [
-                  _buildDayOfWork('Monday', '10:00 AM - 6:00 PM'),
-                  _buildDayOfWork('Tuesday', '10:00 AM - 6:00 PM'),
-                  _buildDayOfWork('Wednesday', '10:00 AM - 6:00 PM'),
-                  _buildDayOfWork('Thursday', '10:00 AM - 6:00 PM'),
-                  _buildDayOfWork('Friday', '10:00 AM - 6:00 PM'),
-                  _buildDayOfWork('Saturday', '10:00 AM - 4:00 PM'),
-                  // Add more days of work as needed
+                  Expanded(
+                    child: _buildDaysOfWork(),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: _buildWorkTimes(),
+                  ),
                 ],
               ),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors
-                    .accentClr, 
-              ),
-              child: const Text(
-                'Edit Store Info', 
-                style: TextStyle(
-                  color: Colors.white, 
-                ),
-              ),
-            ),
+            const SizedBox(height: 30),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildDayOfWork(String day, String time) {
+  Widget _buildDaysOfWork() {
+    return Wrap(
+      spacing: 8,
+      children: [
+        _buildDayOfWork('Monday'),
+        _buildDayOfWork('Tuesday'),
+        _buildDayOfWork('Wednesday'),
+        _buildDayOfWork('Thursday'),
+        _buildDayOfWork('Friday'),
+        _buildDayOfWork('Saturday'),
+        _buildDayOfWork('Sunday'),
+      ],
+    );
+  }
+
+  Widget _buildDayOfWork(String day) {
     return Chip(
-      label: Text('$day: $time'),
+      label: Text(day),
+      backgroundColor: AppColors.accentClr,
+      labelStyle: const TextStyle(color: Colors.white),
+    );
+  }
+
+  Widget _buildWorkTimes() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildWorkTime('AM', '08:00', '12:00'),
+        _buildWorkTime('PM', '14:00', '18:00'),
+      ],
+    );
+  }
+
+  Widget _buildWorkTime(String period, String startTime, String endTime) {
+    return Chip(
+      label: Text(
+        '$period: $startTime - $endTime',
+        style: const TextStyle(fontSize: 16),
+      ),
       backgroundColor: AppColors.accentClr,
       labelStyle: const TextStyle(color: Colors.white),
     );
   }
 }
+
+
